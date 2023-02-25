ca19eecb5cda7f212fa636362c11194ad28a6c05
lijiawei <ljw@live.jp>
2023-02-25 11:03:58 +0800

    docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,5 @@
 import { h } from 'vue'
 import defaultTheme from 'vitepress/theme';
-import LayoutBottom from './components/LayoutBottom.vue';
 import JobModels from './components/JobModels.vue';
 import './styles/index.css';
 
@@ -8,7 +7,6 @@ export default {
   ...defaultTheme,
   Layout() {
     return h(defaultTheme.Layout, null, {
-      'layout-bottom': () => h(LayoutBottom),
       'home-features-before': () => h(JobModels),
     });
   },
