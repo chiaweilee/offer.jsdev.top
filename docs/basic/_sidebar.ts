98172cba4046a32ded3e5234c47692fa2c259e42
plus <ljw@live.jp>
2023-02-27 13:44:31 +0800

    Update _sidebar.ts

--- docs/basic/_sidebar.ts
+++ docs/basic/_sidebar.ts
@@ -1,6 +1,9 @@
 export default [
   {
     text: 'JavaScript 基础',
-    items: [{ text: '数据类型', link: '/basic/javascript/data-types' }],
+    items: [
+      { text: '数据类型', link: '/basic/javascript/data-types' },
+      { text: 'ES6', link: '/basic/javascript/es6' },
+    ],
   },
 ];
