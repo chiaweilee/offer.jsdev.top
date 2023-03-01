ba62c19a9d627d92b7ec4e6f499d21e6d2ce8f24
plus <ljw@live.jp>
2023-03-01 15:49:45 +0800

    Update _sidebar.ts

--- docs/algorithm/_sidebar.ts
+++ docs/algorithm/_sidebar.ts
@@ -2,7 +2,8 @@ export default [
   {
     text: '位运算',
     items: [
-      { text: '.', link: '/algorithm/bit-manipulation/' },
+      { text: '137. 只出现一次的数字 II', link: '/algorithm/bit-manipulation/single-number-ii' },
+      { text: '260. 只出现一次的数字 III', link: '/algorithm/bit-manipulation/single-number-iii' },
     ],
   },
 ];
