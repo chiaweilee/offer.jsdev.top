845dcb8384c2b6b4c44c8f2a53dbf498beea1d95
lijiawei <ljw@live.jp>
2023-03-02 22:05:00 +0800

    feat: gh-count

---
+++ docs/.vitepress/theme/components/GithubStar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+if (!document.querySelector('.gh-count')) {
+  fetch('https://api.github.com/repos/html-js/html-js.github.io')
+    .then((response) => response.json())
+    .then((response) => {
+      if (typeof response?.stargazers_count === 'number') {
+        const action = document.createElement('a');
+        action.className = 'gh-count';
+        action.rel = 'noreferrer';
+        action.target = '_blank';
+        action.href = 'https://github.com/html-js/html-js.github.io';
+        action.textContent = response.stargazers_count.toLocaleString('en-US');
+        const container = document.querySelector('.VPHome .VPHero.VPHomeHero .container .actions');
+        container?.appendChild(action);
+      }
+    });
+}
+</script>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import { h } from 'vue'
 import defaultTheme from 'vitepress/theme';
+import GithubStar from './components/GithubStar.vue';
 import JobModelLabel from './components/JobModelLabel.vue';
 import JobModels from './components/JobModels.vue';
 import References from './components/References.vue';
@@ -9,6 +10,7 @@ export default {
   ...defaultTheme,
   Layout() {
     return h(defaultTheme.Layout, null, {
+      'home-hero-after': () => h(GithubStar),
       'home-features-before': () => h(JobModelLabel),
       'home-features-after': () => h(JobModels),
       'doc-footer-before': () => h(References),
