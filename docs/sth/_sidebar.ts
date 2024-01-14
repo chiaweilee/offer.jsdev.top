bcea17f90f638183cc6ff77e39149011929f37a1
lijiawei <ljw@live.jp>
2024-01-14 13:27:35 +0800

    feat: sth

--- docs/sth/_sidebar.ts
+++ docs/sth/_sidebar.ts
@@ -2,8 +2,17 @@ export default [
   {
     text: 'API',
     items: [
-      { text: 'instanceOf', link: '/sth/instanceOf' },
-      { text: 'typeOf', link: '/sth/typeOf' },
+      {
+        text: '推荐使用',
+        items: [
+          { text: 'instanceOf', link: '/sth/instanceOf' },
+          { text: 'typeOf', link: '/sth/typeOf' },
+        ],
+      },
+      {
+        text: '其他',
+        link: '/sth/others'
+      },
     ],
   },
 ];
