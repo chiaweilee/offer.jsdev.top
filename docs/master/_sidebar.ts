cf2dd953a59e6ac6506aaeeee87c2d8f74c4f4a7
lijiawei <ljw@live.jp>
2024-01-29 22:10:40 +0800

    docs: DevOps

--- docs/master/_sidebar.ts
+++ docs/master/_sidebar.ts
@@ -4,10 +4,15 @@ export default [
     items: [],
   },
   {
-    text: '管理',
+    text: '项目管理',
     items: [
       { text: '项目管理', link: '/master/pm' },
       { text: 'Scrum 敏捷开发', link: '/master/scrum' },
+      { text: 'DevOps', link: '/master/devops' },
     ],
+  },
+  {
+    text: '团队管理',
+    items: [],
   },
 ];
