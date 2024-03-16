d1df7bb74bc4910ff49b737a7606ec3d1ea940b7
plus <ljw@live.jp>
2024-03-16 13:48:04 +0800

    docs

--- docs/interview/_sidebar.ts
+++ docs/interview/_sidebar.ts
@@ -1,5 +1,11 @@
 export default [
   {
+    text: '面试',
+    items: [
+      { text: '自我介绍', link: '/interview/introduce' },
+    ],
+  },
+  {
     text: '面试题',
     items: [
       { text: '手写代码', link: '/interview/handwrite' },
